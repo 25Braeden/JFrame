cbf96210f058f03eb033947472d2a193429cd2d1
Braeden Audlin <125329807+25Braeden@users.noreply.github.com>
2024-05-29 08:18:20 -0400

    Update problem3.java

--- src/problem3/problem3.java
+++ src/problem3/problem3.java
@@ -1,49 +1,10 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2761
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 CourierNewPSMT;\f1\froman\fcharset0 Times-Roman;\f2\fmodern\fcharset0 CourierNewPS-ItalicMT;
-}
-{\colortbl;\red255\green255\blue255;\red195\green123\blue90;\red23\green23\blue26;\red71\green149\blue242;
-\red174\green176\blue183;\red0\green0\blue0;\red185\green101\blue173;\red38\green157\blue169;}
-{\*\expandedcolortbl;;\cssrgb\c81176\c55686\c42745;\cssrgb\c11765\c12157\c13333;\cssrgb\c33725\c65882\c96078;
-\cssrgb\c73725\c74510\c76863;\cssrgb\c0\c0\c0;\cssrgb\c78039\c49020\c73333;\cssrgb\c16471\c67451\c72157;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs26\fsmilli13333 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 public void \cf4 \strokec4 paint\cf5 \strokec5 (Graphics g)
-\f1\fs24 \cf0 \strokec6 \
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs26\fsmilli13333 \cf5 \strokec5 \{
-\f1\fs24 \cf0 \strokec6 \
-
-\f0\fs26\fsmilli13333 \cf5 \strokec5 \'a0\'a0\'a0g.setColor(Color.
-\f2\i \cf7 \strokec7 yellow
-\f0\i0 \cf5 \strokec5 );
-\f1\fs24 \cf0 \strokec6 \
-
-\f0\fs26\fsmilli13333 \cf5 \strokec5 \'a0\'a0\'a0g.fillOval(\cf8 \strokec8 350\cf5 \strokec5 , \cf8 \strokec8 300\cf5 \strokec5 , \cf8 \strokec8 300\cf5 \strokec5 , \cf8 \strokec8 300\cf5 \strokec5 );
-\f1\fs24 \cf0 \strokec6 \
-
-\f0\fs26\fsmilli13333 \cf5 \strokec5 \'a0\'a0\'a0g.setColor(Color.
-\f2\i \cf7 \strokec7 blue
-\f0\i0 \cf5 \strokec5 );
-\f1\fs24 \cf0 \strokec6 \
-
-\f0\fs26\fsmilli13333 \cf5 \strokec5 \'a0\'a0\'a0g.fillOval(\cf8 \strokec8 425\cf5 \strokec5 , \cf8 \strokec8 400\cf5 \strokec5 , \cf8 \strokec8 50\cf5 \strokec5 , \cf8 \strokec8 50\cf5 \strokec5 );
-\f1\fs24 \cf0 \strokec6 \
-
-\f0\fs26\fsmilli13333 \cf5 \strokec5 \'a0\'a0\'a0g.fillOval(\cf8 \strokec8 525\cf5 \strokec5 , \cf8 \strokec8 400\cf5 \strokec5 , \cf8 \strokec8 50\cf5 \strokec5 , \cf8 \strokec8 50\cf5 \strokec5 );
-\f1\fs24 \cf0 \strokec6 \
-
-\f0\fs26\fsmilli13333 \cf5 \strokec5 \'a0\'a0\'a0g.setColor(Color.
-\f2\i \cf7 \strokec7 red
-\f0\i0 \cf5 \strokec5 );
-\f1\fs24 \cf0 \strokec6 \
-
-\f0\fs26\fsmilli13333 \cf5 \strokec5 \'a0\'a0\'a0g.drawArc(\cf8 \strokec8 425\cf5 \strokec5 , \cf8 \strokec8 450\cf5 \strokec5 , \cf8 \strokec8 150\cf5 \strokec5 , \cf8 \strokec8 100\cf5 \strokec5 , \cf8 \strokec8 0\cf5 \strokec5 , -\cf8 \strokec8 180\cf5 \strokec5 );
-\f1\fs24 \cf0 \strokec6 \
-
-\f0\fs26\fsmilli13333 \cf5 \strokec5 \}
-\f1\fs24 \cf0 \strokec6 \
+public void paint(Graphics g)
+{
+   g.setColor(Color.yellow);
+   g.fillOval(350, 300, 300, 300);
+   g.setColor(Color.blue);
+   g.fillOval(425, 400, 50, 50);
+   g.fillOval(525, 400, 50, 50);
+   g.setColor(Color.red);
+   g.drawArc(425, 450, 150, 100, 0, -180);
 }
